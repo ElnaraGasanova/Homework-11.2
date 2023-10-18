c5ed1c6e34d52e9cbf60b77c7ad7c5cc4b7dfc6d
ElnaraGasanova <elnaraskypro@gmail.com>
2023-10-19 01:48:47 +0300

    Добавлена функция str_title и Докстринги к обеим функциям.

--- str_func.py
+++ str_func.py
@@ -1,6 +1,23 @@
 def str_upper(data: str):
+
+    """Функция возвращает строку со
+    всеми заглавными буквами"""
+
     data_up = data.upper()
     return data_up
 
+
 if __name__ == "__main__":
-    print(str_upper("hello"))
+    print(str_upper("заглавные все буквы"))
+
+def str_title(data: str):
+
+    """Функция возвращает заглавными
+    первые буквы каждого слова в строке"""
+
+    data_tit = data.title()
+    return data_tit
+
+
+if __name__ == "__main__":
+    print(str_title("заглавные первые буквы"))
